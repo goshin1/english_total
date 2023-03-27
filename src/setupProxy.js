2c14b2b1067c15f2971fd861a21765a4662ad0ca
shin1 <gci787@naver.com>
2023-03-27 21:06:20 +0900

    express서버에단어전송

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = (app) => {
     app.use(
         '/add',
         createProxyMiddleware({
-            target : 'http://localhost:5000/',
-            //target : 'https://port-0-english-server-3vw25lch3mal1.gksl2.cloudtype.app/',
+            //target : 'http://localhost:5000/',
+            target : 'https://port-0-english-server-3vw25lch3mal1.gksl2.cloudtype.app/',
             changeOrigin : true,
         })
     );
